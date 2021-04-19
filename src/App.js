32341290338698379a5c2a563ae8724bd06c7858
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-04-19 12:24:41 +0200

    added simple style for layout and get value of inputSearch in our Searchbar

--- src/App.js
+++ src/App.js
@@ -19,14 +19,8 @@ class CountDownTimer extends React.Component {
   }
 
   componentDidMount(){
-    // console.log('Start Coundown', {nazwa:this.props.name})
-
     this.intervalId=setInterval(()=>{
-
-      // console.log('Start Timer')
-     
-
-      this.setState((prevState)=>{
+        this.setState((prevState)=>{
 
         if (prevState.time === 1 ) {
           clearInterval(this.intervalId)
@@ -72,6 +66,8 @@ class App extends React.Component{
     this.inputFieldText= React.createRef();
     this.inputFieldTime= React.createRef();
 
+    this.inputSearchValue=React.createRef();
+
     this.state = {
       listTasks: [],
     }
@@ -107,32 +103,50 @@ class App extends React.Component{
 
   }
 
+  // Szukanie elementów 
+
+  handleSearchValue =()=>{
+    const valueOfSearchinput= this.inputSearchValue.current.value;
+    console.log(valueOfSearchinput)
+  }
+
 
   render(){
     return(
       <div className='App'>
-        <h1>Timer`s to Do Lists</h1>
-        <form>
-            <input ref={this.inputFieldText} type="text" placeholder="your task for today"></input>
-            <input ref={this.inputFieldTime} type="number" placeholder="for how long"></input>
-        </form>
-        <button onClick={this.handlerAddTask}>Add task</button>
-        <button onClick={this.handleDeleteAllTasks}>Clean tasks</button>
-        <ol>
-            {
-              this.state.listTasks.map((elem)=>{
-                return (
-                  <li>
-                    <CountDownTimer deleteTask={this.handleDeleteTask} name={elem.name} time={elem.time} />
-                  </li>
-                )                                  
-              })
-            }
-          </ol>
-      
+        <div className="layout">
+
+          <div className="flex-left">
+                <h1>Timer`s to Do Lists</h1>
+                <form>
+                    <input ref={this.inputFieldText} type="text" placeholder="your task for today"></input>
+                    <input ref={this.inputFieldTime} type="number" placeholder="for how long"></input>
+                </form>
+                <button onClick={this.handlerAddTask}>Add task</button>
+                <button onClick={this.handleDeleteAllTasks}>Clean tasks</button>
+                <ol>
+                    {
+                      this.state.listTasks.map((elem)=>{
+                        return (
+                          <li>
+                            <CountDownTimer deleteTask={this.handleDeleteTask} name={elem.name} time={elem.time} />
+                          </li>
+                        )                                  
+                      })
+                    }
+                  </ol>
+            </div>
+              
+            <div className="flex-righ">
+              <input ref={this.inputSearchValue} onChange={this.handleSearchValue} type="text" placeholder="Search.."></input>
+              <button >Usuń</button>
+              <ol></ol>        
+             </div>
+
+
+          </div>
       </div>
     )
   }
-
 }
 export default App;

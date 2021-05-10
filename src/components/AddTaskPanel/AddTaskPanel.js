d3552f5343b9453beaaded6666b86160c9be9748
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-05-10 15:26:57 +0200

    markup Panel Add task : added markup for adding tasks and added functionality for controlled component

--- src/components/AddTaskPanel/AddTaskPanel.js
+++ src/components/AddTaskPanel/AddTaskPanel.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
+import './AddTaskPanel.css'
 
 
 
 class AddTaskPanel extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            text: '',
+            number: '',
+        }
+    }
+
+    handleText = (e) => {
+        this.setState({
+            text: e.target.value
+        })
+    }
+
+    handleTime = (e) => {
+        this.setState({
+            number: e.target.value
+        })
+    }
+
+    // Dodawanie Tasku
+
+    handleClickTask = () => {
+
     }
     render() {
         return (
             <div>
-                <h2>Dodaj Task</h2>
-            </div>);
+                <h1>-Task-Management-🔥</h1>
+                <h2>What are your top tasks today?</h2>
+                <form className=''>
+                    <input type='text' placeholder='Add your task' value={this.state.text} onChange={this.handleText}></input>
+                    <input type='number' placeholder='For how long ?' value={this.state.number} onChange={this.handleTime}></input>
+                </form>
+                <button onClick={this.handleClickTask}>Add your Task</button>
+            </div>
+        );
     }
 }
 
